37ec23a9d912c8c1f4cdcc995939366533beabda
Vivian Zhu <vivianzhu88@gmail.com>
2023-11-19 08:20:14 +0300

    add cleaned data

---
+++ cleaned_data/preproc.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+#image paths
+s_bundesliga = pd.read_csv("trades_data\"1-bundesliga.csv")
+s_champ = pd.read_csv("trades_data\championship.csv")
+s_eredivisie = pd.read_csv("trades_data\eredivisie.csv")
+s_liganos = pd.read_csv("trades_data\liga-nos.csv")
+s_ligue1 = pd.read_csv("trades_data\ligue-1.csv")
+s_premierleague = pd.read_csv("trades_data\premier-league.csv")
+s_premierliga = pd.read_csv("trades_data\premier-liga.csv")
+s_premeradiv = pd.read_csv("trades_data\premera-division.csv")
+s_seriea = pd.read_csv("trades_data\serie-a.csv")
+
+t_appearances = pd.read_csv("transfermarkt_data\appearances.csv")
+t_clubgames = pd.read_csv("transfermarkt_data\club_games.csv")
+t_clubs = pd.read_csv("transfermarkt_data\clubs.csv")
+t_comps = pd.read_csv("transfermarkt_data\competitions.csv")
+t_events = pd.read_csv("transfermarkt_data\game_events.csv")
+t_games = pd.read_csv("transfermarkt_data\games.csv")
+t_playerval = pd.read_csv("transfermarkt_data\player_valuations.csv")
+t_players = pd.read_csv("transfermarkt_data\players.csv")
+ 
+
+
+
+
+
+
+
+
+
